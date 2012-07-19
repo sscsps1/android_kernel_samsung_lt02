681d9b51b936d22ca2daa68d7ec2db7a828d406e
Tejun Heo <tj@kernel.org>
2012-07-19 13:52:53 -0700

    kthread_worker: reimplement flush_kthread_work() to allow freeing the work item being executed
    commit 46f3d976213452350f9d10b0c2780c2681f7075b upstream.
    
    kthread_worker provides minimalistic workqueue-like interface for
    users which need a dedicated worker thread (e.g. for realtime
    priority).  It has basic queue, flush_work, flush_worker operations
    which mostly match the workqueue counterparts; however, due to the way
    flush_work() is implemented, it has a noticeable difference of not
    allowing work items to be freed while being executed.
    
    While the current users of kthread_worker are okay with the current
    behavior, the restriction does impede some valid use cases.  Also,
    removing this difference isn't difficult and actually makes the code
    easier to understand.
    
    This patch reimplements flush_kthread_work() such that it uses a
    flush_work item instead of queue/done sequence numbers.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Cc: Colin Cross <ccross@google.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- kernel/kthread.c
+++ kernel/kthread.c
@@ -382,7 +382,7 @@ static void insert_kthread_work(struct kthread_worker *worker,
 	lockdep_assert_held(&worker->lock);
 
 	list_add_tail(&work->node, pos);
-	work->queue_seq++;
+	work->worker = worker;
 	if (likely(worker->task))
 		wake_up_process(worker->task);
 }
@@ -457,7 +457,6 @@ retry:
 	else
 		noop = true;
 
-<<<<<<< HEAD
 	spin_unlock_irq(&worker->lock);
 
 	if (!noop)
@@ -465,8 +464,7 @@ retry:
 }
 EXPORT_SYMBOL_GPL(flush_kthread_work);
 
-=======
->>>>>>> 34b6567... kthread_worker: reorganize to prepare for flush_kthread_work() reimplementation
+
 /**
  * flush_kthread_worker - flush all current works on a kthread_worker
  * @worker: worker to flush
